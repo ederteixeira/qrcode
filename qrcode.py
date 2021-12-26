427c98353cd3f4e62946aa3d11238de21accb00a
ederteixeira <ccbeder@gmail.com>
2021-12-26 00:28:59 -0300

    interface gui

--- qrcode.py
+++ qrcode.py
@@ -1,5 +1,23 @@
+from tkinter import *
 import pyqrcode
 
-url = pyqrcode.create('https://github.com/ederteixeirahttps://github.com/ederteixeira')
-url.svg('github.svg', scale=6)
-url.png('github.png', scale=6)
+def gerador():
+    criador = textoins.get()
+    url = pyqrcode.create(criador)
+    url.svg('qrcode.svg', scale=6)
+    url.png('qrcode.png', scale=6)
+
+app = Tk()
+app.geometry("400x250")
+app.title("Gerador QRCode")
+
+texto = Label(app, text="Informe o site:")
+texto.grid(column=0, row=0)
+
+textoins = Entry(app)
+textoins.grid(column=1, row=0)
+
+botao =  Button(app, text="Gerar", command=gerador)
+botao.grid(column=2, row=0)
+
+app.mainloop()
